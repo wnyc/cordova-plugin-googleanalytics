f3921a97c60b8b8abb835d5ef221b98536b041f9
Brad Kammin <bkammin@nypublicradio.org>
2014-05-15 10:23:20 -0400

    turn off debug statements

--- src/ios/GoogleAnalyticsPlugin.m
+++ src/ios/GoogleAnalyticsPlugin.m
@@ -20,7 +20,7 @@
     // Optional: set Google Analytics dispatch interval to e.g. 20 seconds.
     [GAI sharedInstance].dispatchInterval = 30;
     // Optional: set debug to YES for extra debugging information.
-    [GAI sharedInstance].debug = YES;
+    //[GAI sharedInstance].debug = YES;
     
     NSString * gaKey = @"__GA_KEY__";
     if( gaKey!=nil && [gaKey isEqualToString:@""]==NO && [gaKey rangeOfString:@"GA_KEY"].location == NSNotFound ){
