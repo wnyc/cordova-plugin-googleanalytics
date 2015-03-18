53dac30e1f46facf3bce0e875e78c5ab837ff422
Brad Kammin <bkammin@nypublicradio.org>
2015-03-18 13:51:28 -0400

    WNYCAPP-332 add configuration methods to js

--- www/googleanalytics.js
+++ www/googleanalytics.js
@@ -5,6 +5,14 @@ var exec = require("cordova/exec");
  */    
 var GoogleAnalytics = function(){};
 
+GoogleAnalytics.prototype.setDefaultCategory = function(category, success, error) {
+  exec(success, error, "GoogleAnalyticsPlugin", "setdefaultcategory", [category]);
+};
+
+GoogleAnalytics.prototype.setUseHeartbeat = function(useHeartbeat, success, error) {
+  exec(success, error, "GoogleAnalyticsPlugin", "setuseheartbeat", [useHeartbeat]);
+};
+
 GoogleAnalytics.prototype.logevent = function(success, error, category, action, label, value) {
   exec(success, error, "GoogleAnalyticsPlugin", "logevent", [category, action, label, value]);
 };
