9f720f32847086f0aee69be6e3a64dc2c043c729
Brad Kammin <bkammin@nypublicradio.org>
2015-03-19 13:45:57 -0400

    WNYCAPP-332 added session heartbeat for iOS

--- src/ios/GoogleAnalyticsPlugin.m
+++ src/ios/GoogleAnalyticsPlugin.m
@@ -7,36 +7,88 @@
 #import "GAIFields.h"
 #import "GAIDictionaryBuilder.h"
 
+static int const kGoogleAnalyticsPluginHeartbeatInterval= 60;
+static NSString *const kGoogleAnalyticsPluginDefaultCategory = @"Unknown";
+static NSString *const kGoogleAnalyticsPluginHeartbeatAction = @"SessionHeartbeat";
+
+@interface GoogleAnalyticsPlugin ()
+
+@property BOOL useHeartbeat;
+@property NSString *defaultCategory;
+@property NSTimer *heartbeatTimer;
+
+@end;
+
 @implementation GoogleAnalyticsPlugin
 
 #pragma mark Initialization
 
-- (void)pluginInitialize
-{
+- (void)pluginInitialize {
+    self.useHeartbeat = NO;
+    self.defaultCategory = kGoogleAnalyticsPluginDefaultCategory;
+    
     // Optional: automatically send uncaught exceptions to Google Analytics.
     [GAI sharedInstance].trackUncaughtExceptions = YES;
     // Optional: set Google Analytics dispatch interval to e.g. 20 seconds.
     [GAI sharedInstance].dispatchInterval = 30;
-    // Optional: set debug to YES for extra debugging information.
-    //[GAI sharedInstance].debug = YES;
+    // Optional: uncomment for extra debugging information.
+    //[[GAI sharedInstance].logger setLogLevel:kGAILogLevelVerbose];
     
-    NSString * gaKey = @"__GA_KEY__";
+    NSString *gaKey = @"__GA_KEY__";
     if( gaKey!=nil && [gaKey isEqualToString:@""]==NO && [gaKey rangeOfString:@"GA_KEY"].location == NSNotFound ){
         // Create tracker instance. This tracker can later be retreived by calling defaultTracker
         [[GAI sharedInstance] trackerWithTrackingId:gaKey];
+        
+        // manually start new GA session
+        [self _startNewSession];
     }
     
-    NSLog(@"Google Analytics Plugin initialized");
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_onResignActive) name:UIApplicationWillResignActiveNotification object:nil];
+}
+
+#pragma mark Cleanup
+
+- (void)dispose {
+    if (self.useHeartbeat) {
+        [self _stopHeartbeat];
+    }
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
 }
 
 #pragma mark Plugin methods
 
-- (void)logscreenview:(CDVInvokedUrlCommand*)command
-{
+- (void)setuseheartbeat:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* pluginResult = nil;
+    BOOL useHeartbeat = [[command.arguments objectAtIndex:0] boolValue];
+    
+    if (self.useHeartbeat != useHeartbeat) {
+        self.useHeartbeat = useHeartbeat;
+    
+        if (self.useHeartbeat) {
+            [self _startHeartbeat];
+        }
+        else {
+            [self _stopHeartbeat];
+        }
+    }
+    
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setdefaultcategory:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* pluginResult = nil;
+    NSString *defaultCategory = [command.arguments objectAtIndex:0];
+    
+    self.defaultCategory = defaultCategory;
+    
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)logscreenview:(CDVInvokedUrlCommand *)command {
     CDVPluginResult* pluginResult = nil;
     NSString* screen = [command.arguments objectAtIndex:0];
-    
-    NSLog (@"Google Analytics logging screen: (%@)", screen );
     
     [self _logScreenView:screen];
     
@@ -44,7 +96,7 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void) logevent:(CDVInvokedUrlCommand*)command {
+- (void) logevent:(CDVInvokedUrlCommand *)command {
     CDVPluginResult* pluginResult = nil;
     if( [command.arguments count] < 3 ) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"GA events require at least a Category string, an Action string, and a Label string."];
@@ -55,8 +107,6 @@
     NSString * action = [command.arguments objectAtIndex:1];
     NSString * label = [command.arguments objectAtIndex:2];
     NSNumber * value = [NSNumber numberWithInt:0];
-    
-    NSLog (@"Google Analytics logging event: (%@)", action );
     
     if( [command.arguments count] > 3 ) {
         NSString * valueString = [command.arguments objectAtIndex:3];
@@ -83,5 +133,57 @@
     [tracker send:[[GAIDictionaryBuilder createAppView] build]];
     [tracker set:kGAIScreenName value:nil];
 }
+
+#pragma mark Heartbeat timer methods
+
+- (void) _startHeartbeat {
+    if (self.heartbeatTimer) {
+        [self.heartbeatTimer invalidate];
+    }
+
+    // start the timer
+    self.heartbeatTimer = [NSTimer scheduledTimerWithTimeInterval: kGoogleAnalyticsPluginHeartbeatInterval
+                                                           target: self
+                                                         selector: @selector(_heartbeat)
+                                                         userInfo: nil
+                                                          repeats: YES];
+    // send initial heartbeat
+    [self _heartbeat];
+}
+
+- (void) _stopHeartbeat {
+    if (self.heartbeatTimer) {
+        if ([self.heartbeatTimer isValid]) {
+            // send final heartbeat
+            [self _heartbeat];
+        }
+
+        [self.heartbeatTimer invalidate];
+        self.heartbeatTimer = nil;
+    }
+}
+
+- (void) _heartbeat {
+    [self _logGAEvent:self.defaultCategory action:kGoogleAnalyticsPluginHeartbeatAction label:nil value:0];
+}
+
+#pragma App state change handlers
+
+- (void)onAppTerminate {
+    [[GAI sharedInstance] dispatch];
+}
+
+- (void)_onResignActive {
+    [[GAI sharedInstance] dispatch];
+}
+
+#pragma mark Session methods
+
+- (void)_startNewSession {
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAISessionControl value:@"start"];
+    [tracker set:kGAISessionControl value:nil];
+}
+
 
 @end
